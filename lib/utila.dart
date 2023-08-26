85a3e7cfcb8e1e1033a5ba454be579cecf958334
Amos <igefad12@gmail.com>
2023-08-26 14:25:30 +0100

    depends on savaser

--- lib/utila.dart
+++ lib/utila.dart
@@ -1,38 +1,3 @@
 library utila;
 
 export 'package:savaser/savaser.dart';
-/* // CONTROLLERS
-export 'package:utila/controllers/contact_controller.dart';
-
-// UTILS
-export 'package:utila/utils/validators/db_validator.dart';
-export 'package:utila/utils/validators/number_form_field_validator.dart';
-
-export 'package:utila/utils/config.dart';
-export 'package:utila/utils/country_util.dart';
-export 'package:utila/utils/navigation_util.dart';
-export 'package:utila/utils/toast_util.dart';
-export 'package:utila/utils/utils.dart';
-
-// RES
-export 'package:utila/res/countries.dart'; */
-
-//WIDGETS
-//export 'package:utila/';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
-
-
-
-/* 
-- countries (res)
-- validators
-- contry_util
-- nav util
-- toast util
-
- */
